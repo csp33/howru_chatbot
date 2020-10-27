5f932a63b325c34710715408fdfc605876621e52
Carlos Sánchez <carlos.sanchez@4iq.com>
2020-10-27 20:39:36 +0100

    fix logger path

--- log/logger.py
+++ log/logger.py
@@ -1,5 +1,5 @@
 import logging
 
 logging.basicConfig(format='%(asctime)-15s %(processName)-8s [%(threadName)s] %(levelname)-8s %(message)s',
-                    level=logging.INFO, filename='/home/carlossanchezpaez/log/howru_chatbot.log')
+                    level=logging.INFO, filename='/var/log/howru_chatbot.log')
 logger = logging.getLogger()
